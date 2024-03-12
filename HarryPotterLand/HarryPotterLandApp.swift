6435340efb78b0289d37b1b364bc399675373a6d
Patrycja Dziabala <patrycja.dziabala@gmail.com>
2024-03-12 14:48:13 +0100

    Initial Commit

---
+++ HarryPotterLand/HarryPotterLandApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterLandApp.swift
+//  HarryPotterLand
+//
+//  Created by Patka on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterLandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
